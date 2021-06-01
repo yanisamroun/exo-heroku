ce7f9a63b5ad703d694a4a44a42e5d735c610c46
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-01 14:40:20 +0200

    Add views with engine

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 // Importer express
 const express = require('express');
+const path = require('path');
 
 // Initialiser l'application
 const app = express();
+
+// Lier mon express.JS à un moteur de template (ici: twig)
+// 1. On va récupérer mes templates dans le dossier views
+app.set('views', path.join(__dirname,'views'));
+// 2. Connecter twig à express.js
+app.set('view engine', 'twig');
 
 // Définir le port dans une variable
 const port = 4400;
 
 // Routing
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index', {bachi: "Mange tes morts"});
 })
 
-app.get('/services', (req,res) => {
-    res.send('Mes services');
+app.get('/blog', (req,res) => {
+    res.send('Mes articles');
+})
+
+app.get('/blog/:id', (req,res) => {
+    res.send(req.params.id)
 })
 
 // Lance le serveur
