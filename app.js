be17f3299ba6c0386a101a4f2d36b22dca798800
yanisamroun <80908466+yanisamroun@users.noreply.github.com>
2021-06-15 12:13:52 +0200

    firste commite

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'twig');
 app.use(express.static('public'))
 
 // Définir le port dans une variable
-const PORT = 3000;
+const port = 3000;
 
 // Routing
 app.get('/', (req, res) => {
